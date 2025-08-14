4ddb9cf784f420cdd252b2a56fdf7c1b6bd7d0d1
JUNGJAE119 <wjdwo102@gmail.com>
2025-08-14 20:21:44 +0900

    Update MemberMapper.java
    commit test2

--- src/main/java/com/aiaca/btop/member/mapper/MemberMapper.java
+++ src/main/java/com/aiaca/btop/member/mapper/MemberMapper.java
@@ -6,6 +6,4 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface MemberMapper {
     void createMember(MemberInfo memberInfo);
-    void deleteMember(MemberInfo memberInfo);
-
 }
