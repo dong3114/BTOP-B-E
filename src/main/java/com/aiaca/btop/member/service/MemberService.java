8b40691b73ca92336933e83b7f0e914ebb432b68
dong3114 <167489282+dong3114@users.noreply.github.com>
2025-08-17 16:14:53 +0900

    first commit sj

--- src/main/java/com/aiaca/btop/member/service/MemberService.java
+++ src/main/java/com/aiaca/btop/member/service/MemberService.java
@@ -1,7 +1,22 @@
 package com.aiaca.btop.member.service;
 
+import com.aiaca.btop.member.domain.LoginInfo;
 import com.aiaca.btop.member.domain.MemberInfo;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface MemberService {
+    // 1. 회원가입 관련
     int createMember(MemberInfo memberInfo);
+
+    // 2. 로그인 관련
+    int validateLogin(LoginInfo loginInfo);
+    String getMemberNo(String memberId, String memberPw);
+
+    // 3. 회원 정보 조회
+    List<MemberInfo> getAllMemberList();
+    MemberInfo getMemberInfo(String memberNo);
+
+
 }
